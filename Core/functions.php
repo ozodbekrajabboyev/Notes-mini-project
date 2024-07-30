da1c7364f569c92749bcdfbfac5272baf83e2cd0
Ozodbek <ozodbek2625@gmail.com>
2024-07-30 11:45:14 +0500

    Building better router

--- Core/functions.php
+++ Core/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Response;
+
 function dd($value)
 {
     echo "<pre>";
@@ -15,9 +16,18 @@ function urlIs($value)
     return $_SERVER['REQUEST_URI'] === $value;
 }
 
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require base_path("views/{$code}.php");
+
+    die();
+}
+
 function authorize($condition, $status = Response::FORBIDDEN)
 {
-    if (! $condition) {
+    if (!$condition) {
         abort($status);
     }
 }
