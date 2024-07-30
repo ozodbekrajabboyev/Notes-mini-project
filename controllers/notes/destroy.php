f1a4da6ebae877ffcdb5d1f9fc1919de0a9bd316
Ozodbek <ozodbek2625@gmail.com>
2024-07-30 12:05:24 +0500

    Little clean-up

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -4,16 +4,17 @@ use Core\Database;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
-
 $currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id']
+    'id' => $_POST['id'],
 ])->findOrFail();
 
 authorize($note['user_id'] === $currentUserId);
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
+$db->query('DELETE FROM notes WHERE id=:id', [
+    'id' => $_POST['id'],
 ]);
+
+header('location: /notes');
+exit();
