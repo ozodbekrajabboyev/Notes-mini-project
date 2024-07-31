097f8a3bf36eb2fdd294d9c0c4db31219f2cf8fe
Ozodbek <ozodbek2625@gmail.com>
2024-07-31 12:25:00 +0500

    Completing CRUD

---
+++ views/notes/edit.view.php
@@ -0,0 +1,49 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="mt-5 md:col-span-2 md:mt-0">
+                <form method="POST" action="/note">
+                    <input type="hidden" name="_method" value="PATCH">
+                    <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                    <div class="shadow sm:overflow-hidden sm:rounded-md">
+                        <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                            <div>
+                                <label for="body" class="block text-sm font-medium text-gray-700">Body</label>
+
+                                <div class="mt-1">
+                                    <textarea id="body" name="body" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="Here's an idea for a note..."><?= $note['body'] ?></textarea>
+
+                                    <?php if (isset($errors['body'])) : ?>
+                                        <p class="text-red-500 text-xs mt-2"><?= $errors['body'] ?></p>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                            <a href="/notes" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                                Cancel
+                            </a>
+                            <button type="submit" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                                Update
+                            </button>
+
+                            <button type="button" class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-base px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900" onclick="document.querySelector('#delete-form').submit()">Delete</button>
+                            
+                        </div>
+                    </div>
+                </form>
+                <form id="delete-form" class="hidden" method="POST" action="/note">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                </form>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php require base_path('views/partials/footer.php') ?>
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,11 +10,11 @@
 
         <p><?= htmlspecialchars($note['body']) ?></p>
 
-        <form class="mt-4" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?>">
-            <button class="font-bold text-sm text-red-500">Delete</button>
-        </form>
+        <footer class="mt-6">
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">Edit</a>
+        </footer>
+
+        
     </div>
 </main>
 
