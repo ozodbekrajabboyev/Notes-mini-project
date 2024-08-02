1dbd71da4caa4abe2c752747b09099e6cd5178e4
Ozodbek <ozodbek2625@gmail.com>
2024-08-02 11:49:40 +0500

    Refactoring works

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -3,6 +3,7 @@
 use Core\App;
 use Core\Database;
 use Core\Validator;
+use Http\Forms\LoginForm;
 
 // Log the user in if the credentials match.
 $db = App::resolve(Database::class);
@@ -10,19 +11,10 @@ $db = App::resolve(Database::class);
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
-
-if (!Validator::email($email)) {
-    $errors['email'] = 'Provide a valid email address.';
-}
-
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password';
-}
-
-if (!empty($errors)) {
-    return view('session/create.view.php', [
-        'errors' => $errors
+$form = new LoginForm();
+if (!$form->validate($email, $password)){
+    return view("session/create.view.php",[
+        "errors" => $form->errors(),
     ]);
 }
 
