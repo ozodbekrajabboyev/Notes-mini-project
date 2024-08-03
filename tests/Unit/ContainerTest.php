de4056fa464d23f39106f4ceec2186001a31d99c
Ozodbek <ozodbek2625@gmail.com>
2024-08-03 20:30:10 +0500

    Testing

---
+++ tests/Unit/ContainerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Core\Container;
+
+
+
+test('it can resolve something out of the container', function () {
+    $container = new Container();
+
+    $container->bind('foo', fn() => 'bar');
+
+    // act
+    $result = $container->resolve('foo');
+
+    // assert/expect
+    expect($result)->toEqual('bar');
+    
+});
