16108eb13ada122cf1d3f47d386320b9d8afbec5
Ozodbek <ozodbek2625@gmail.com>
2024-08-03 12:11:41 +0500

    Small enhancement

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -15,7 +15,7 @@
 
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="">
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" value="<?= old('email') ?>">
 
                             <?php if (isset($errors['email'])) : ?>
                                 <p class="font-bold text-red-500 text-xs mt-2"><?= $errors['email'] ?></p>
