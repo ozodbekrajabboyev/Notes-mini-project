511dda0ca47c71a8e771f159c6efbb022b10fbef
Ozodbek <ozodbek2625@gmail.com>
2024-11-01 09:58:27 +0500

    logo added successfully

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,7 +3,9 @@
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="flex-shrink-0">
-                    <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+                    <a href="/">
+                        <img class="h-8 w-8 bg-[#F3F4F6] w-100 bg-origin-padding" src="https://www.svgrepo.com/show/7827/notes.svg" alt="Your Company">
+                    </a>
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
